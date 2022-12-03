c3d1e208cdad631cb6fee1425dbb1bb46a9255f3
Max <elenquentaro@gmail.com>
2022-12-03 08:01:08 +0300

    some infrastruct improves

--- src/AdventCalendar.cs
+++ src/AdventCalendar.cs
@@ -5,5 +5,9 @@ namespace advent_of_code {
         public static AdventDay GetCurrentDay() {
             return new Day1();
         }
+
+        public static string GetDefaultPathForDay(AdventDay day) {
+            return string.Format("resources/input/2022-{0}.txt", day.DayNumber);
+        }
     }
 }
--- src/AdventDay.cs
+++ src/AdventDay.cs
@@ -1,5 +1,6 @@
 ﻿namespace advent_of_code {
     internal abstract class AdventDay {
+        public virtual int DayNumber { get => 1; }
         public abstract void SolvePart1(string[] inputLines);
         public abstract void SolvePart2(string[] inputLines);
     }

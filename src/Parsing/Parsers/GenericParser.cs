63adaa0e22aeca9cf4236554d39410ed806075df
Max <elenquentaro@gmail.com>
2022-12-04 08:56:57 +0300

    Great Mighty My Own Parse Implementation
    (another among many similar)

---
+++ src/Parsing/Parsers/GenericParser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace advent_of_code.Parsing.Parsers {
+    public abstract class GenericParser<T> : IParser<T> {
+        public abstract T Parse(string input);
+
+        object IParser.Parse(string input) {
+            return Parse(input);
+        }
+
+        public virtual string[] SplitInput(string input) {
+            return input.Split(GetDefaultSeparator());
+        }
+
+        public virtual char GetDefaultSeparator() {
+            // space by default
+            return ' ';
+        }
+    }
+}
---
+++ src/Parsing/Utils/ParseUtils.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace advent_of_code.Parsing.Utils {
+    internal static class ParseUtils {
+        public static IEnumerable<(string first, string second)> IteratePairs(string[] inputLines, char splitChar) {
+            return inputLines.Select(line => SplitToPair(line, splitChar));
+        }
+
+        public static (string, string) SplitToPair(string input, char splitChar) {
+            var arr = input.Split(splitChar);
+            // TODO: think about not stuffy safety keeping. again
+            return (arr[0], arr[1]);
+        }
+
+        public static IEnumerable<(T first, T second)> IterateParsedPairs<T>(string[] inputLines, IParser<T> parser, char baseSplitChar = ' ') {
+            return IteratePairs(inputLines, baseSplitChar)
+                .Select(parser.ParsePair);
+        }
+
+        public static (T first, T second) ParsePair<T>(this IParser<T> parser, (string, string) pair) {
+            return (parser.Parse(pair.Item1), parser.Parse(pair.Item2));
+        }
+    }
+}
